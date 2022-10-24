def060400704f6ee3d3871fdeab0900981f6643b
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-10-24 15:26:36 +0400

    implement directus `link`

--- repository/directus/link.go
+++ repository/directus/link.go
@@ -2,11 +2,61 @@ package directus
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"net/url"
 
 	"github.com/mgjules/mgjules-go/entity"
 )
 
+type Link struct {
+	ID           string  `json:"id"`
+	Name         string  `json:"name"`
+	URL          string  `json:"url"`
+	Icon         string  `json:"icon"`
+	AlternateURL *string `json:"alternate_url"`
+	NewWindow    bool    `json:"new_window"`
+}
+
+func (l Link) ToEntity() entity.Link {
+	return entity.Link{
+		ID:           l.ID,
+		Name:         l.Name,
+		URL:          l.URL,
+		Icon:         l.Icon,
+		AlternateURL: l.AlternateURL,
+		NewWindow:    l.NewWindow,
+	}
+}
+
 func (db *Directus) GetLinks(ctx context.Context) ([]entity.Link, error) {
-	return nil, errors.New("not implemented")
+	var result Result[[]Link]
+	resp, err := db.client.R().
+		SetQueryParamsFromValues(url.Values{
+			"fields": []string{
+				"id",
+				"name",
+				"url",
+				"icon",
+				"alternate_url",
+				"new_window",
+			},
+			"status": []string{"published"},
+			"sort":   []string{"sort"},
+		}).
+		SetResult(&result).
+		Get("/items/link")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get meta: %w", err)
+	}
+
+	if resp.IsError() {
+		return nil, fmt.Errorf("failed to get meta: response code %d", resp.StatusCode())
+	}
+
+	links := make([]entity.Link, len(result.Data))
+	for i, link := range result.Data {
+		links[i] = link.ToEntity()
+	}
+
+	return links, nil
 }
