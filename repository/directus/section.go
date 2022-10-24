c3c02b4b225386f438e7d50b41eac89ad5140a89
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-10-24 19:44:32 +0400

    implement directus `sections`

--- repository/directus/section.go
+++ repository/directus/section.go
@@ -2,11 +2,52 @@ package directus
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"net/url"
 
 	"github.com/mgjules/mgjules-go/entity"
 )
 
+type Section struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Icon string `json:"icon"`
+}
+
+func (s Section) ToEntity() entity.Section {
+	return entity.Section{
+		ID:   s.ID,
+		Name: s.Name,
+		Icon: s.Icon,
+	}
+}
+
 func (db *Directus) GetSections(ctx context.Context) ([]entity.Section, error) {
-	return nil, errors.New("not implemented")
+	var result Result[[]Section]
+	resp, err := db.client.R().
+		SetQueryParamsFromValues(url.Values{
+			"fields": []string{
+				"id",
+				"name",
+				"icon",
+			},
+			"status": []string{"published"},
+			"sort":   []string{"sort"},
+		}).
+		SetResult(&result).
+		Get("/items/section")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sections: %w", err)
+	}
+
+	if resp.IsError() {
+		return nil, fmt.Errorf("failed to get sections: response code %d", resp.StatusCode())
+	}
+
+	sections := make([]entity.Section, len(result.Data))
+	for i, section := range result.Data {
+		sections[i] = section.ToEntity()
+	}
+
+	return sections, nil
 }
