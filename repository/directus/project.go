5876f2ee9ee154c0207f9d90847a856632441842
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-10-24 20:46:06 +0400

    implement directus `projects`

--- repository/directus/project.go
+++ repository/directus/project.go
@@ -2,11 +2,66 @@ package directus
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"net/url"
 
 	"github.com/mgjules/mgjules-go/entity"
 )
 
+type Project struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	Link         string `json:"link"`
+	Technologies []struct {
+		Technology Technology `json:"technology"`
+	} `json:"technologies"`
+}
+
+func (p Project) ToEntity() entity.Project {
+	technologies := make([]entity.Technology, len(p.Technologies))
+	for i, technology := range p.Technologies {
+		technologies[i] = technology.Technology.ToEntity()
+	}
+
+	return entity.Project{
+		ID:           p.ID,
+		Name:         p.Name,
+		Description:  p.Description,
+		Link:         p.Link,
+		Technologies: technologies,
+	}
+}
+
 func (db *Directus) GetProjects(ctx context.Context) ([]entity.Project, error) {
-	return nil, errors.New("not implemented")
+	var result Result[[]Project]
+	resp, err := db.client.R().
+		SetQueryParamsFromValues(url.Values{
+			"fields": []string{
+				"id",
+				"name",
+				"description",
+				"link",
+				"technologies.*.id",
+				"technologies.*.name",
+			},
+			"status": []string{"published"},
+			"sort":   []string{"sort"},
+		}).
+		SetResult(&result).
+		Get("/items/project")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get projects: %w", err)
+	}
+
+	if resp.IsError() {
+		return nil, fmt.Errorf("failed to get projects: response code %d", resp.StatusCode())
+	}
+
+	projects := make([]entity.Project, len(result.Data))
+	for i, project := range result.Data {
+		projects[i] = project.ToEntity()
+	}
+
+	return projects, nil
 }
