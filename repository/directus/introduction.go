25c66d76ebd55d5518e51fe6595086d3d751b7c5
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2022-10-24 17:00:03 +0400

    implement directus `introduction`

--- repository/directus/introduction.go
+++ repository/directus/introduction.go
@@ -2,11 +2,48 @@ package directus
 
 import (
 	"context"
-	"errors"
+	"fmt"
+	"net/url"
 
 	"github.com/mgjules/mgjules-go/entity"
 )
 
+type Introduction struct {
+	ID           string `json:"id"`
+	Introduction string `json:"introduction"`
+	Avatar       string `json:"avatar"`
+}
+
+func (i Introduction) ToEntity(directusURL string) entity.Introduction {
+	return entity.Introduction{
+		ID:           i.ID,
+		Introduction: i.Introduction,
+		Avatar:       directusURL + "/assets/" + i.Avatar + "/avatar.webp?key=intro",
+	}
+}
+
 func (db *Directus) GetIntroduction(ctx context.Context, id string) (*entity.Introduction, error) {
-	return nil, errors.New("not implemented")
+	var result Result[Introduction]
+	resp, err := db.client.R().
+		SetQueryParamsFromValues(url.Values{
+			"fields": []string{
+				"id",
+				"introduction",
+				"avatar",
+			},
+			"limit": []string{"1"},
+		}).
+		SetResult(&result).
+		Get("/items/introduction")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get introduction: %w", err)
+	}
+
+	if resp.IsError() {
+		return nil, fmt.Errorf("failed to get introduction: response code %d", resp.StatusCode())
+	}
+
+	intro := result.Data.ToEntity(db.directusURL)
+
+	return &intro, nil
 }
