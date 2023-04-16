f48e7368e2253b1984fd028778135d1ceb76351a
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2023-04-16 22:34:16 +0400

    add comment to config

--- config/config.go
+++ config/config.go
@@ -7,19 +7,25 @@ import (
 )
 
 type Config struct {
+	// General
 	Prod bool `envconfig:"PROD"`
 
+	// HTTP server
 	ServerHost      string `envconfig:"SERVER_HOST" default:"0.0.0.0"`
 	ServerPort      int    `envconfig:"SERVER_PORT" default:"13337"`
 	ServerTLSDomain string `envconfig:"SERVER_TLS_DOMAIN"`
 
+	// Authentication
 	AuthToken string `envconfig:"AUTH_TOKEN" required:"true"`
 
+	// EdgeDB (deprecated)
 	EdgeDBDSN string `envconfig:"EDGEDB_DSN"`
 
+	// Directus
 	DirectusURL   string `envconfig:"DIRECTUS_URL"`
 	DirectusToken string `envconfig:"DIRECTUS_TOKEN"`
 
+	// Dartsass ?
 	DartSassEmbeddedBinary string `envconfig:"DART_SASS_EMBEDDED_BINARY" default:"dart-sass-embedded"`
 }
 
