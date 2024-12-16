6f28d4905dc8cb45f424731286c2d3dae81a9da0
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2024-12-16 13:46:02 +0400

    static moved

--- unocss.config.ts
+++ unocss.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
           // "./templates/**/*.scss",
           "./templates/**/*.templ",
         ],
-        outFile: "./static/css/style.css",
+        outFile: "./internal/static/css/style.css",
       },
     ],
   },
