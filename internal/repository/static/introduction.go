cd597cd09e0ab02ba2b42b317025c0d2025dfe10
Michaël Giovanni Jules <julesmichaelgiovanni@gmail.com>
2025-08-03 06:54:56 +0400

    Update introduction.go

--- internal/repository/static/introduction.go
+++ internal/repository/static/introduction.go
@@ -9,7 +9,7 @@ import (
 func (db *static) GetIntroduction(ctx context.Context, id string) (*entity.Introduction, error) {
 	return &entity.Introduction{
 		ID: id,
-		Introduction: `Highly accomplished Senior Software Engineer with over 10 years of experience in 
+		Introduction: `Pragmatic Senior Software Engineer with over 10 years of experience in 
     backend development of which 5 years specializing in Go. Also, a definite cat lover.`,
 		Avatar: "/img/avatar.webp",
 	}, nil
